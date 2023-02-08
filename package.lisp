a96fd5471ae51172e820f3e7720948301b88a977
kamysheblid <kamysheblid@gmail.com>
2023-02-08 17:49:12 +0330

    removed useless code

--- package.lisp
+++ package.lisp
@@ -2,5 +2,4 @@
 
 (defpackage #:xft-fix
   (:use #:cl :xft :stumpwm)
-;  (:import-from #:stumpwm #:screen-fonts #:*timer-list*)
   (:export #:start-loop #:*clear-font-repeat* #:*clear-font-delay* #:*clear-font-cache-timer*))
